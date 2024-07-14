c82d7bda28de7e9e3fddbf6ded533cccda81b879
ghaithatfeh <85496434+ghaithatfeh@users.noreply.github.com>
2024-07-14 21:34:24 +0300

    Fix API client class

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -19,7 +19,7 @@ class APIClient<T> {
 		this.endpoint = endpoint;
 	}
 
-	getAll(config: AxiosRequestConfig) {
+	getAll = (config: AxiosRequestConfig) => {
 		return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then((res) => res.data);
 	}
 }
