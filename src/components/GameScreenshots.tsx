c06f5daa6c571a9fd42fd39c6d80363477214491
ghaithatfeh <85496434+ghaithatfeh@users.noreply.github.com>
2024-08-17 18:57:03 +0300

    Build game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,27 @@
+import useScreenshot from "../hooks/useScreenshots";
+import { Image, SimpleGrid, Spinner } from "@chakra-ui/react";
+
+interface Props {
+	gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+	const { data, error, isLoading } = useScreenshot(gameId);
+
+	if (isLoading) return <Spinner />;
+
+	if (error) throw error;
+
+	console.log(data);
+	
+
+	return (
+		<SimpleGrid columns={[null, 1, 2, 3]} spacing={5}>
+			{data?.results.map(({ id, image }) => (
+				<Image key={id} src={image} />
+			))}
+		</SimpleGrid>
+	);
+};
+
+export default GameScreenshots;
---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query"
+import APIClient from "../services/api-clients"
+import { Screenshot } from "../entities/Screenshot"
+
+const useScreenshot = (id: number) => {
+    const apiClient = new APIClient<Screenshot>("/games/" + id + "/screenshots")
+    return  useQuery({
+        queryKey: ['screenshots', id],
+        queryFn: apiClient.getAll
+    })
+}
+
+export default useScreenshot;
